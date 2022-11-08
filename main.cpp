c90412b26f0243e5b7b7a93f42709f77b735f97b
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-11-08 14:46:22 -0300

    Testando métodos virtuais no main

--- main.cpp
+++ main.cpp
@@ -4,13 +4,28 @@ using std::cout;
 #include <vector>
 
 #include "Forma.h"
+#include "Forma.cpp"
 
 #include "Quadrado.h"
+#include "Quadrado.cpp"
 
 
 int main(int argc, char **argv)
 {
 	Forma *formaPtr;
+	Quadrado *quadradoPtr;
+
+	formaPtr = new Quadrado();	
+	quadradoPtr = new Quadrado();
+	cout << "Calculando a area\n";
+	cout << "formaPtr\n";
+	cout << formaPtr->calcArea( ) << '\n';
+	cout << "quadradoPtr\n";
+	cout << quadradoPtr->calcArea( ) << '\n';
 	
+
+	delete formaPtr;	
+	delete quadradoPtr;	
+
 	return 0;
 }
